7d6b972339b1e57987838cc99f265391f7ab0c15
Vikas Mandal <vikasmandal380@gmail.com>
2025-03-11 22:52:18 +0530

    Introduction to Data Analysis using SQL

---
+++ Write Your First SQL Query.sql
@@ -0,0 +1,6 @@
+select * from restaurants;
+select price, calories from food_items;
+select order_id,customer_id,total_price from orders;
+select count(restaurant_name) from restaurants;
+select distinct cuisine from restaurants;
+select distinct item_name from food_items;
